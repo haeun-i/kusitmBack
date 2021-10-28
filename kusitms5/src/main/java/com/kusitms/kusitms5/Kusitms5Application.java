f153c72af4487710aeda30a023b03ef165f851a0
eunhee <dmsgml9903@gmail.com>
2021-10-28 16:37:01 +0900

    setting

---
+++ kusitms5/src/main/java/com/kusitms/kusitms5/Kusitms5Application.java
@@ -0,0 +1,13 @@
+package com.kusitms.kusitms5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kusitms5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kusitms5Application.class, args);
+	}
+
+}
